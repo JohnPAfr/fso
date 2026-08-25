7d2d380515b2c739cf2d01f3efafb26590f0e8a5
JohnPAfr <john.aurry@proton.me>
2026-08-25 14:06:21 +0200

    fix: typo

--- part3/models/mongo.js
+++ part3/models/mongo.js
@@ -18,26 +18,4 @@ personSchema.set("toJSON", {
   },
 });
 
-// if (process.argv.length === 3) {
-//   Person.find({}).then((result) => {
-//     if (result.length > 0) {
-//       console.log("phonebook:");
-//       result.forEach((person) => {
-//         console.log(`${person.name} ${person.number}`);
-//       });
-//       mongoose.connection.close();
-//     }
-//   });
-// } else {
-//   const person = new Person({
-//     name: process.argv[3],
-//     number: process.argv[4],
-//   });
-
-//   person.save().then((result) => {
-//     console.log(`added ${person.name} number ${person.number} to phonebook`);
-//     mongoose.connection.close();
-//   });
-// }
-
-module.export = mongoose.model("Person", personSchema);
+module.exports = mongoose.model("Person", personSchema);
