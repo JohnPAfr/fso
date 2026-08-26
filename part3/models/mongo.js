c8bec99527e10e80bb80b5479f62218651c89f6c
JohnPAfr <john.aurry@proton.me>
2026-08-26 16:15:23 +0200

    fix: type

--- part3/models/mongo.js
+++ part3/models/mongo.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-mongoose.set('strictQuery', false)
-mongoose.connect(url, { family: 4 })
+mongoose.set("strictQuery", false);
+mongoose.connect(url, { family: 4 });
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
   number: {
-    type: Number,
+    type: String,
     minLength: 8,
     validate: {
       validator: function (v) {
-        return /^(?=.{8,}$)\d{2,3}-\d+$/.test(v)
+        return /^(?=.{8,}$)\d{2,3}-\d+$/.test(v);
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
     required: true,
   },
-})
+});
 
-personSchema.set('toJSON', {
+personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
-})
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema);
